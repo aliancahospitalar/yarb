dc09d25becb060df8e4ba98f4d9eefb5d6ed701b
Michelle Lim <michelle.lim@yale.edu>
2017-06-07 12:56:15 -0300

    hash bundle files

--- internals/config/webpack.config.js
+++ internals/config/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
     output: {
         path: path.join(__dirname, '../../dist'),
-        filename: '[name].entry.js',
+        filename: '[name].[hash].entry.js',
         publicPath: '/static/'
     },
     watch: true,
